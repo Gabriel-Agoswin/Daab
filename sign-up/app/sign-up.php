6dad484f21f55beccaea67f3e1e5eef430fbd9e7
Gabriel-Agoswin <Gabriela.salifu@gmail.com>
2024-09-27 23:39:32 +0000

    commit on sign-up file commentted the api snippet

--- sign-up/app/sign-up.php
+++ sign-up/app/sign-up.php
@@ -1,19 +1,18 @@
 
 
 <?php
+/*
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-
-client.messages
-  .create({
+    client.messages.create({
     body: 'You have an appointment with Owl, Inc. on Friday, November 3 at 4:00 PM. Reply C to confirm.',
     messagingServiceSid: 'MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
     to: '+15558675310'
   })
   .then(message => console.log(message.sid));
-
+*/
 
 ?>
 
